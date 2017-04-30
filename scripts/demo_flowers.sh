8caec8bec8a9e18bb9ab01cc3f58b8f81aafdaba
Sudhir Nallam <psn240@nyu.edu>
2017-04-30 19:56:28 -0400

    Testing code

--- scripts/demo_flowers.sh
+++ scripts/demo_flowers.sh
@@ -1,4 +1,5 @@
-. CONFIG
+FLOWERS_NET_TXT=/mnt/data/lm_sje_flowers_c10_hybrid_0.00070_1_10_trainvalids.txt_iter16400.t7
+CHECKPOINT_DIR=/mnt/checkpoints
 
 checkpoint_dir=${CHECKPOINT_DIR} \
 net_gen=flowers_v2_nc1_cls0.5_int1.0_ngf128_ndf128_200_net_G.t7 \
